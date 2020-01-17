71b4a74891858a73ad35d303f773f12b946bd66f
Caleb Hiebert <calebkhiebert@gmail.com>
2020-01-17 09:16:58 -0600

    update response block

--- response_blocks.go
+++ response_blocks.go
@@ -1,6 +1,7 @@
 package convai
 
 type ResponseBlock struct {
-	Type string `json:"t" mapstructure:"t" msgpack:"t"`
-	Data string `json:"d" mapstructure:"d" msgpack:"d"`
+	Type     string `json:"type" mapstructure:"type" msgpack:"type"`
+	Data     string `json:"data" mapstructure:"data" msgpack:"data"`
+	Position int    `json:"pos" mapstructure:"pos" msgpack:"pos"`
 }
