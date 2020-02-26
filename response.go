425c7c6b5435bf802d187622b66408ecfa22077e
Caleb Hiebert <calebkhiebert@gmail.com>
2020-02-26 12:25:02 -0600

    add phone and card

--- response.go
+++ response.go
@@ -29,18 +29,44 @@ type MessageConfig struct {
 }
 
 type XMLResponse struct {
-	XMLName      xml.Name `xml:"response" json:"-" msgpack:"-" mapstructure:"-"`
-	InnerXML     string   `xml:",innerxml" json:"-" msgpack:"-" mapstructure:"-"`
-	Message      string   `json:"message" msgpack:"message" mapstructure:"message"`
-	QuickReplies []XMLQR  `xml:"qr" json:"quickReplies" msgpack:"quickReplies" mapstructure:"quickReplies"`
+	XMLName         xml.Name            `xml:"response" json:"-" msgpack:"-" mapstructure:"-"`
+	InnerXML        string              `xml:",innerxml" json:"-" msgpack:"-" mapstructure:"-"`
+	Message         string              `json:"message" msgpack:"message" mapstructure:"message"`
+	QuickReplies    []XMLQR             `xml:"qr" json:"quickReplies" msgpack:"quickReplies" mapstructure:"quickReplies"`
+	Phone           []XMLPhone          `xml:"phone" json:"phone" msgpack:"phone"`
+	Cards           []XMLCard           `xml:"card" json:"cards" msgpack:"cards"`
+	CardCollections []XMLCardCollection `xml:"cards" json:"cardCollections" msgpack:"cardCollections"`
 }
 
 type XMLQR struct {
 	XMLName  xml.Name `xml:"qr" json:"-" msgpack:"-" mapstructure:"-"`
-	InnerXML string   `xml:",innerxml" json:"text" msgpack:"text" mapstructure:"text"`
+	Text     string   `xml:",innerxml" json:"text" msgpack:"text" mapstructure:"text"`
 	Value    *string  `xml:"value,attr,omitempty" json:"value,omitempty" msgpack:"value,omitempty" mapstructure:"value,omitempty"`
 	Phone    bool     `xml:"phone,attr,omitempty" json:"phone,omitempty" msgpack:"phone,omitempty" mapstructure:"phone,omitempty"`
 	Email    bool     `xml:"email,attr,omitempty" json:"email,omitempty" msgpack:"email,omitempty" mapstructure:"email,omitempty"`
 	Image    *string  `xml:"image,attr,omitempty" json:"image,omitempty" msgpack:"image,omitempty" mapstructure:"image,omitempty"`
 	ImageURL *string  `xml:"imageURL,attr,omitempty" json:"imageUrl,omitempty" msgpack:"imageUrl,omitempty" mapstructure:"imageUrl,omitempty"`
+}
+
+type XMLPhone struct {
+	XMLName xml.Name `xml:"phone" json:"-" msgpack:"-" mapstructure:"-"`
+	Number  string   `xml:",innerxml" json:"number" msgpack:"number" mapstructure:"number"`
+	Display *string  `xml:"display,omitempty" json:"display,omitempty" msgpack:"display,omitempty"`
+}
+
+type XMLCardCollection struct {
+	XMLName xml.Name  `xml:"cards" json:"-" msgpack:"-" mapstructure:"-"`
+	Cards   []XMLCard `xml:"card" json:"cards" msgpack:"cards"`
+}
+
+type XMLCard struct {
+	XMLName  xml.Name  `xml:"card" json:"-" msgpack:"-" mapstructure:"-"`
+	Title    string    `xml:"title" json:"title" msgpack:"title" mapstructure:"title"`
+	Subtitle *string   `xml:"subtitle,omitempty" json:"subtitle,omitempty" msgpack:"subtitle,omitempty" mapstructure:"subtitle,omitempty"`
+	Image    *XMLImage `xml:"image,omitempty" json:"image,omitempty" msgpack:"image,omitempty"`
+}
+
+type XMLImage struct {
+	XMLName xml.Name `xml:"image" json:"-" msgpack:"-" mapstructure:"-"`
+	ID      string   `xml:"id,attr" json:"id" msgpack:"id"`
 }
