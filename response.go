908dd193ef8949a9baf9ebfebd17a6195361b086
Caleb Hiebert <calebkhiebert@gmail.com>
2020-02-26 11:04:04 -0600

    add extra message fields

--- response.go
+++ response.go
@@ -1,5 +1,7 @@
 package convai
 
+import "encoding/xml"
+
 type Response struct {
 	Messages []Message `json:"messages" mapstructure:"messages" msgpack:"messages"`
 }
@@ -17,10 +19,28 @@ type Message struct {
 	GraphID     *int64          `json:"graphId" mapstructure:"graphId" msgpack:"graphId"`
 	NodeID      *int64          `json:"nodeId" mapstructure:"nodeId" msgpack:"nodeId"`
 	Blocks      []ResponseBlock `json:"blocks" mapstructure:"blocks" msgpack:"blocks"`
+	Data        XMLResponse     `json:"data" mapstructure:"data" msgpack:"data"`
 	Seq         int             `json:"seq" mapstructure:"seq" msgpack:"seq"`
 }
 
 type MessageConfig struct {
 	Text       string  `json:"text" mapstructure:"text" msgpack:"text"`
 	TypingTime float64 `json:"typingTime" mapstructure:"typingTime" msgpack:"typingTime"`
+}
+
+type XMLResponse struct {
+	XMLName      xml.Name `xml:"response" json:"-" msgpack:"-" mapstructure:"-"`
+	InnerXML     string   `xml:",innerxml" json:"-" msgpack:"-" mapstructure:"-"`
+	Message      string   `json:"message" msgpack:"message" mapstructure:"message"`
+	QuickReplies []XMLQR  `xml:"qr" json:"quickReplies" msgpack:"quickReplies" mapstructure:"quickReplies"`
+}
+
+type XMLQR struct {
+	XMLName  xml.Name `xml:"qr" json:"-" msgpack:"-" mapstructure:"-"`
+	InnerXML string   `xml:",innerxml" json:"text" msgpack:"text" mapstructure:"text"`
+	Value    *string  `xml:"value,attr,omitempty" json:"value,omitempty" msgpack:"value,omitempty" mapstructure:"value,omitempty"`
+	Phone    bool     `xml:"phone,attr,omitempty" json:"phone,omitempty" msgpack:"phone,omitempty" mapstructure:"phone,omitempty"`
+	Email    bool     `xml:"email,attr,omitempty" json:"email,omitempty" msgpack:"email,omitempty" mapstructure:"email,omitempty"`
+	Image    *string  `xml:"image,attr,omitempty" json:"image,omitempty" msgpack:"image,omitempty" mapstructure:"image,omitempty"`
+	ImageURL *string  `xml:"imageURL,attr,omitempty" json:"imageUrl,omitempty" msgpack:"imageUrl,omitempty" mapstructure:"imageUrl,omitempty"`
 }
